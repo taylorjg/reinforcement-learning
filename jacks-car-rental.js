6109ae9e2f447c21b4c54aeec56a82379f0f8e37
taylorjg <jonathan.taylor.programmer@gmail.com>
2020-11-15 23:08:07 +0000

    Got it working

--- jacks-car-rental.js
+++ jacks-car-rental.js
@@ -1,14 +1,9 @@
 const range = n => Array.from(Array(n).keys())
-const add = (a, b) => a + b
 const mul = (a, b) => a * b
 const inc = a => a + 1
 const factorial = n => range(n).map(inc).reduce(mul, 1)
-const sum = xs => xs.reduce(add, 0)
 const zipWithIndex = xs => xs.map((x, index) => [x, index])
 const clamp = (min, max, value) => Math.min(max, Math.max(min, value))
-
-const makeState = (noc1, noc2) => `${noc1}:${noc2}`
-const parseState = s => s.split(':').map(Number)
 
 const poissonProbability = (n, lambda) => {
   const numerator = Math.pow(lambda, n)
@@ -22,28 +17,29 @@ const EXPECTED_RENTAL_RETURNS_LOCATION_1 = 3
 const EXPECTED_RENTAL_RETURNS_LOCATION_2 = 2
 
 const NUMBERS_OF_CARS = range(21)
-const S = NUMBERS_OF_CARS.flatMap(noc1 =>
-  NUMBERS_OF_CARS.map(noc2 =>
-    makeState(noc1, noc2)))
 
 const makePoissonDistribution = lambda =>
   zipWithIndex(NUMBERS_OF_CARS.map(n => poissonProbability(n, lambda)))
-    .filter(([p]) => p > 1e-3)
+    .filter(([p]) => p > 0.001)
 
 const PS1 = makePoissonDistribution(EXPECTED_RENTAL_REQUESTS_LOCATION_1)
 const PS2 = makePoissonDistribution(EXPECTED_RENTAL_REQUESTS_LOCATION_2)
 const PS3 = makePoissonDistribution(EXPECTED_RENTAL_RETURNS_LOCATION_1)
 const PS4 = makePoissonDistribution(EXPECTED_RENTAL_RETURNS_LOCATION_2)
 
+const makeState = (noc1, noc2) => `${noc1}:${noc2}`
+const parseState = s => s.split(':').map(Number)
+
+const S = NUMBERS_OF_CARS.flatMap(noc1 =>
+  NUMBERS_OF_CARS.map(noc2 =>
+    makeState(noc1, noc2)))
 const A = range(11).map(a => a - 5) // [-5, -4, ..., 4, 5]
 const V = new Map(S.map(s => [s, 0]))
 
 const GAMMA = 0.9
-const THETA = 0.1
+const THETA = 1
 
 const probabilitiesNextStatesRewards = (s, a) => {
-  const moved1to2 = a > 0 ? a : 0
-  const moved2to1 = a < 0 ? -a : 0
   const carsMoved = Math.abs(a)
   const [noc1, noc2] = parseState(s)
   const results = []
@@ -51,12 +47,12 @@ const probabilitiesNextStatesRewards = (s, a) => {
     for (const [p2, requested2] of PS2) {
       for (const [p3, returned1] of PS3) {
         for (const [p4, returned2] of PS4) {
-          const noc1New = clamp(0, 20, noc1 - requested1 + returned1 - moved1to2 + moved2to1)
-          const noc2New = clamp(0, 20, noc2 - requested2 + returned2 - moved2to1 + moved1to2)
+          const rented1 = Math.min(noc1 - a, requested1)
+          const rented2 = Math.min(noc2 + a, requested2)
+          const noc1New = clamp(0, 20, noc1 - a - rented1 + returned1)
+          const noc2New = clamp(0, 20, noc2 + a - rented2 + returned2)
           const p = p1 * p2 * p3 * p4
           const s2 = makeState(noc1New, noc2New)
-          const rented1 = Math.min(noc1, requested1)
-          const rented2 = Math.min(noc2, requested2)
           const carsRented = rented1 + rented2
           const r = carsRented * 10 - carsMoved * 2
           results.push({ p, s2, r })
@@ -117,7 +113,6 @@ const improvePolicy = pi => {
       }
     })
     m.set(s, aMax)
-    console.log({ s, aOld, aMax, vMax })
     if (aMax !== aOld) {
       stable = false
     }
@@ -141,14 +136,18 @@ const dumpPolicy = pi => {
   }
   console.log()
   console.log(lastLineBits.join(' '))
+  console.log()
 }
 
 const main = () => {
-  const pi1 = createInitialPolicy()
-  evaluatePolicy(pi1)
-  const [pi2, stable] = improvePolicy(pi1)
-  dumpPolicy(pi2)
-  console.log(`stable: ${stable}`)
+  let pi = createInitialPolicy()
+  for (; ;) {
+    evaluatePolicy(pi) // => updates state value function, V
+    const [pi2, stable] = improvePolicy(pi)
+    if (stable) break
+    pi = pi2
+    dumpPolicy(pi)
+  }
 }
 
 main()
